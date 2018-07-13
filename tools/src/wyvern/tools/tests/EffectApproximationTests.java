4de053f13a9b69b8ffa743f24e1ad955d8a9f100
Justin Lubin <jlubi333@gmail.com>
2018-07-13 15:33:14 -0400

    Improve test suite

--- tools/src/wyvern/tools/tests/EffectApproximationTests.java
+++ tools/src/wyvern/tools/tests/EffectApproximationTests.java
@@ -4,6 +4,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import wyvern.target.corewyvernIL.effects.Effect;
+import wyvern.target.corewyvernIL.expression.StringLiteral;
+import wyvern.target.corewyvernIL.support.Util;
 import wyvern.tools.imports.extensions.WyvernResolver;
 import wyvern.tools.parsing.coreparser.ParseException;
 import wyvern.tools.tests.suites.RegressionTests;
@@ -31,49 +33,97 @@ public class EffectApproximationTests {
 
     @Test
     public void capabilityPassedIntoFunctorResourceModule() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule1",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("log");
-        TestUtil.doApprox(PATH, "approx.userModule1", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule1", expectedEffectBound);
     }
 
     @Test
     public void capabilityPassedIntoMethodPureModule() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule2",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("log");
-        TestUtil.doApprox(PATH, "approx.userModule2", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule2", expectedEffectBound);
     }
 
     @Test
     public void capabilityExposed() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule3",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("write", "log");
-        TestUtil.doApprox(PATH, "approx.userModule3", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule3", expectedEffectBound);
     }
 
     @Test
     public void importNewEffect() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule4",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("write", "log");
-        TestUtil.doApprox(PATH, "approx.userModule4", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule4", expectedEffectBound);
     }
 
     @Test
     public void effectVarTypeMembers() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule5",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("write");
-        TestUtil.doApprox(PATH, "approx.userModule5", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule5", expectedEffectBound);
     }
 
     @Test
     public void globallyAvailableEffectPureModule() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule6",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("myEffect");
-        TestUtil.doApprox(PATH, "approx.userModule6", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule6", expectedEffectBound);
     }
 
     @Test
     public void globallyAvailableEffectResourceModule() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule7",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("write", "myEffect");
-        TestUtil.doApprox(PATH, "approx.userModule7", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule7", expectedEffectBound);
     }
 
     @Test
     public void nonEmptyGloballyAvailableEffectPureModule() throws ParseException {
+        TestUtil.doTestScriptModularly(
+                PATH,
+                "effectApproximation.testUserModule8",
+                Util.stringType(),
+                new StringLiteral("a")
+        );
         Set<Effect> expectedEffectBound = makeEffectSet("myEffect");
-        TestUtil.doApprox(PATH, "approx.userModule8", expectedEffectBound);
+        TestUtil.doApprox(PATH, "effectApproximation.userModule8", expectedEffectBound);
     }
 }
