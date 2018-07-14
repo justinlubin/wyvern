d5f6d599736fd92b276ce3cf87b06fd3b1dfb963
Justin Lubin <jlubi333@gmail.com>
2018-07-13 20:10:40 -0400

    Move effect approximation state class outside visitor

--- tools/src/wyvern/target/corewyvernIL/astvisitor/EffectApproximationVisitor.java
+++ tools/src/wyvern/target/corewyvernIL/astvisitor/EffectApproximationVisitor.java
@@ -60,53 +60,53 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class EffectApproximationVisitor extends ASTVisitor<EffectApproximationVisitor.State, Set<Effect>> {
-    static class State {
-        private ModuleResolver moduleResolver;
-        private TypeContext cachedStandardContext;
-        private Map<String, ValueType> nominalTypes;
+class State {
+    private ModuleResolver moduleResolver;
+    private TypeContext cachedStandardContext;
+    private Map<String, ValueType> nominalTypes;
 
-        State(ModuleResolver moduleResolver, TypeContext standardContext, List<TypedModuleSpec> dependencies) {
-            this.moduleResolver = moduleResolver;
-            this.cachedStandardContext = standardContext;
-            this.nominalTypes = new HashMap<>();
-            for (TypedModuleSpec dep : dependencies) {
-                this.nominalTypes.put(dep.getDefinedTypeName(), dep.getType());
-            }
-        }
-
-        TypeContext getCachedStandardContext() {
-            return this.cachedStandardContext;
-        }
-
-        ValueType resolveNominalType(NominalType nt) {
-            String name = nt.getTypeMember();
-            ValueType wrapper = nominalTypes.get(name);
-            if (wrapper == null) {
-                return null;
-            }
-            if (!(wrapper instanceof StructuralType)) {
-                throw new RuntimeException("Cannot resolve nominal type: " + nt);
-            }
-            List<DeclType> dts = ((StructuralType) wrapper).getDeclTypes();
-            if (dts.size() != 1) {
-                throw new RuntimeException("Unexpected resolution of nominal type (length != 1): " + nt);
-            }
-            DeclType dt = dts.get(0);
-            if (!(dt instanceof DeclTypeWithResult)) {
-                throw new RuntimeException("Unexpected resolution of nominal type (not DeclTypeWithResult): " + nt);
-            }
-            return ((DeclTypeWithResult) dt).getRawResultType();
-        }
-
-        Module resolveModule(Variable v) {
-            String name = v.getName();
-            int dollarIndex = name.indexOf('$');
-            String qualifiedName = name.substring(dollarIndex + 1);
-            return this.moduleResolver.resolveModule(qualifiedName);
+    State(ModuleResolver moduleResolver, TypeContext standardContext, List<TypedModuleSpec> dependencies) {
+        this.moduleResolver = moduleResolver;
+        this.cachedStandardContext = standardContext;
+        this.nominalTypes = new HashMap<>();
+        for (TypedModuleSpec dep : dependencies) {
+            this.nominalTypes.put(dep.getDefinedTypeName(), dep.getType());
         }
     }
 
+    TypeContext getCachedStandardContext() {
+        return this.cachedStandardContext;
+    }
+
+    ValueType resolveNominalType(NominalType nt) {
+        String name = nt.getTypeMember();
+        ValueType wrapper = nominalTypes.get(name);
+        if (wrapper == null) {
+            return null;
+        }
+        if (!(wrapper instanceof StructuralType)) {
+            throw new RuntimeException("Cannot resolve nominal type: " + nt);
+        }
+        List<DeclType> dts = ((StructuralType) wrapper).getDeclTypes();
+        if (dts.size() != 1) {
+            throw new RuntimeException("Unexpected resolution of nominal type (length != 1): " + nt);
+        }
+        DeclType dt = dts.get(0);
+        if (!(dt instanceof DeclTypeWithResult)) {
+            throw new RuntimeException("Unexpected resolution of nominal type (not DeclTypeWithResult): " + nt);
+        }
+        return ((DeclTypeWithResult) dt).getRawResultType();
+    }
+
+    Module resolveModule(Variable v) {
+        String name = v.getName();
+        int dollarIndex = name.indexOf('$');
+        String qualifiedName = name.substring(dollarIndex + 1);
+        return this.moduleResolver.resolveModule(qualifiedName);
+    }
+}
+
+public class EffectApproximationVisitor extends ASTVisitor<State, Set<Effect>> {
     // Entry point
 
     public static Set<Effect> approximateEffectBound(ModuleResolver moduleResolver, Module module) {
